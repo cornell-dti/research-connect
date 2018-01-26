c0ca1f1cb0d2a788fba145d1f8897225d35d2873
Aram Baghdassarian <abagh0703@gmail.com>
2018-01-25 19:22:42 -0500

    separated yearSelect component

--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import '../index.css';
 
 class Opportunity extends Component {
---
+++ src/components/YearSelect.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+
+class YearSelect extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            yearSelect: this.props.yearSelect
+        };
+        console.log(this.state);
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e) {
+        this.setState({yearSelect: {
+            "Freshman" : this.freshman.checked,
+            "Sophomore" : this.sophomore.checked,
+            "Junior" : this.junior.checked,
+            "Senior" : this.senior.checked
+        }
+        }, () => {
+            this.props.updateYear(this.state.yearSelect);
+        });
+    }
+
+    handleSubmit(event) {
+        var fresh = this.state.Freshman.toString();
+        var soph = this.state.Sophomore.toString();
+        var junior = this.state.Junior.toString();
+        var senior = this.state.Senior.toString();
+        alert('Freshman: ' + fresh + ' Sophomore: ' + soph + ' Junior: ' + junior + ' Senior: ' + senior);
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <input ref={(node) => {this.freshman = node}}  onChange={this.handleChange} type="checkbox" name="Freshman" value="Freshman"/>Freshman
+                <input ref={(node) => {this.sophomore = node}} onChange={this.handleChange} type="checkbox" name="Sophomore" value="Sophomore"/>Sophomore
+                <input ref={(node) => {this.junior = node}} onChange={this.handleChange} type="checkbox" name="Junior" value="Junior"/>Junior
+                <input ref={(node) => {this.senior = node}} onChange={this.handleChange} type="checkbox" name="Senior" value="Senior"/>Senior
+
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
+}
+
+export default YearSelect;
