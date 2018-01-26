c3c3cedd4aade09fc0ac43d84126e9378f5da0b7
Aram Baghdassarian <abagh0703@gmail.com>
2018-01-25 19:25:50 -0500

    finished filtering for year

--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -1,96 +1,99 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import '../index.css';
 
 class Opportunity extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      area: [], //required, area(s) of research (molecular bio, bioengineering, electrical engineering, computer science, etc.)
-      labName: '',    //required
-      labId: '',  //required
-      pi: '', //required
-      supervisor: '', //can be null
-      projectDescription: '', //required, add min length that you see fit
-      undergradTasks: '',  //what the undergrad would be doing, can be null
-      opens: {type: Date, default: new Date()},   //if no date is sent use new Date()
-      closes: {type: Date, default: null},  //null if rolling
-      startDate: '', //null if start asap, string b/c it will prob be something like Fall 2018
-      minSemesters: 0,   //can be null, indicating no min. minimum number of semesters they're expected to work in the lab
-      minHours: 0, //can be null, indicating no minimum
-      maxHours: 0, //can be null, indicating no max
-      qualifications: '', //can be null/empty
-      minGPA: 0, //0 if no minimum gpa required
-      requiredClasses: [], //can be empty
-      questions: [],    //can be empty
-      yearsAllowed: [],  //required, do they accept freshman, sophomores, juniors, and/or seniors
-      //applications: 0,   //number of people who've submitted, default 0, they don't submit this
-      spots: 0
-    };
-  }
-
-    contains(needle) {
-    // Per spec, the way to identify NaN is that it is not equal to itself
-    var findNaN = needle !== needle;
-    var indexOf;
-
-    if(!findNaN && typeof Array.prototype.indexOf === 'function') {
-        indexOf = Array.prototype.indexOf;
-    } else {
-        indexOf = function(needle) {
-            var i = -1, index = -1;
-
-            for(i = 0; i < this.length; i++) {
-                var item = this[i];
-
-                if((findNaN && item !== item) || item.toLowerCase() === needle.toLowerCase()) {
-                    index = i;
-                    break;
-                }
-            }
-
-            return index;
+    constructor(props) {
+        super(props);
+        this.state = {
+            title: '',
+            area: [], //required, area(s) of research (molecular bio, bioengineering, electrical engineering, computer science, etc.)
+            labName: '',    //required
+            labId: '',  //required
+            pi: '', //required
+            supervisor: '', //can be null
+            projectDescription: '', //required, add min length that you see fit
+            undergradTasks: '',  //what the undergrad would be doing, can be null
+            opens: {type: Date, default: new Date()},   //if no date is sent use new Date()
+            closes: {type: Date, default: null},  //null if rolling
+            startDate: '', //null if start asap, string b/c it will prob be something like Fall 2018
+            minSemesters: 0,   //can be null, indicating no min. minimum number of semesters they're expected to work in the lab
+            minHours: 0, //can be null, indicating no minimum
+            maxHours: 0, //can be null, indicating no max
+            qualifications: '', //can be null/empty
+            minGPA: 0, //0 if no minimum gpa required
+            requiredClasses: [], //can be empty
+            questions: [],    //can be empty
+            yearsAllowed: [],  //required, do they accept freshman, sophomores, juniors, and/or seniors
+            //applications: 0,   //number of people who've submitted, default 0, they don't submit this
+            spots: 0
         };
     }
 
-    return indexOf.call(this, needle) > -1;
-};
+    contains(needle) {
+        // Per spec, the way to identify NaN is that it is not equal to itself
+        var findNaN = needle !== needle;
+        var indexOf;
 
-  shouldShow(){
-      const filteredOptions = this.props.filteredOptions;
-      console.log('deep');
-      console.log(this.props.yearsAllowed);
-      if (filteredOptions.yearSelect.Freshman && this.props.yearsAllowed.indexOf("freshman") === -1){
-          return false;
-      }
-      return true;
-  }
+        if (!findNaN && typeof Array.prototype.indexOf === 'function') {
+            indexOf = Array.prototype.indexOf;
+        } else {
+            indexOf = function (needle) {
+                var i = -1, index = -1;
 
-  render() {
-    return (
-      <tr style={{display: this.shouldShow() ? "" : "none"}}>
-          <td>{ this.props.title }</td>
-          <td>{ this.props.area }</td>
-          <td>{ this.props.labName }</td>
-          <td>{ this.props.pi }</td>
-          <td>{ this.props.supervisor }</td>
-          <td>{ this.props.projectDescription }</td>
-          <td>{ this.props.undergradTasks }</td>
-          <td>{ this.props.opens }</td>
-          <td>{ this.props.closes }</td>
-          <td>{ this.props.startDate }</td>
-          <td>{ this.props.minSemesters }</td>
-          <td>{ this.props.minHours }</td>
-          <td>{ this.props.maxHours }</td>
-          <td>{ this.props.qualifications }</td>
-          <td>{ this.props.minGPA }</td>
-          <td>{ this.props.requiredClasses }</td>
-          <td>{ this.props.questions }</td>
-          <td>{ this.props.yearsAllowed }</td>
-          <td>{ this.props.spots }</td>
-      </tr>
-    )
-  }
+                for (i = 0; i < this.length; i++) {
+                    var item = this[i];
+
+                    if ((findNaN && item !== item) || item.toLowerCase() === needle.toLowerCase()) {
+                        index = i;
+                        break;
+                    }
+                }
+
+                return index;
+            };
+        }
+
+        return indexOf.call(this, needle) > -1;
+    };
+
+    shouldShow() {
+        const filteredOptions = this.props.filteredOptions;
+        console.log('deep');
+        console.log(this.props.yearsAllowed);
+        if (filteredOptions.yearSelect.Freshman && this.props.yearsAllowed.indexOf("freshman") === -1 ||
+            filteredOptions.yearSelect.Sophomore && this.props.yearsAllowed.indexOf("sophomore") === -1 ||
+            filteredOptions.yearSelect.Junior && this.props.yearsAllowed.indexOf("junior") === -1 ||
+            filteredOptions.yearSelect.Senior && this.props.yearsAllowed.indexOf("senior") === -1) {
+            return false;
+        }
+        return true;
+    }
+
+    render() {
+        return (
+            <tr style={{display: this.shouldShow() ? "" : "none"}}>
+                <td>{ this.props.title }</td>
+                <td>{ this.props.area }</td>
+                <td>{ this.props.labName }</td>
+                <td>{ this.props.pi }</td>
+                <td>{ this.props.supervisor }</td>
+                <td>{ this.props.projectDescription }</td>
+                <td>{ this.props.undergradTasks }</td>
+                <td>{ this.props.opens }</td>
+                <td>{ this.props.closes }</td>
+                <td>{ this.props.startDate }</td>
+                <td>{ this.props.minSemesters }</td>
+                <td>{ this.props.minHours }</td>
+                <td>{ this.props.maxHours }</td>
+                <td>{ this.props.qualifications }</td>
+                <td>{ this.props.minGPA }</td>
+                <td>{ this.props.requiredClasses }</td>
+                <td>{ this.props.questions }</td>
+                <td>{ this.props.yearsAllowed }</td>
+                <td>{ this.props.spots }</td>
+            </tr>
+        )
+    }
 }
 
 export default Opportunity;
