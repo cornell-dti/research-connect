b19c8ce3b4db470bd6824d773276891484ce75fa
Aram Baghdassarian <abagh0703@gmail.com>
2018-01-29 01:08:48 -0500

    added external json in opportunity.js

--- src/components/Opportunity.js
+++ src/components/Opportunity.js
@@ -1,33 +1,35 @@
 import React, {Component} from 'react';
 import '../index.css';
 import { BrowserRouter as Router } from 'react-router-dom'
+import OpportunityJSON from './Opportunity.json'
 
 class Opportunity extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            title: '',
-            area: [], //required, area(s) of research (molecular bio, bioengineering, electrical engineering, computer science, etc.)
-            labName: '',    //required
-            labId: '',  //required
-            pi: '', //required
-            supervisor: '', //can be null
-            projectDescription: '', //required, add min length that you see fit
-            undergradTasks: '',  //what the undergrad would be doing, can be null
-            opens: {type: Date, default: new Date()},   //if no date is sent use new Date()
-            closes: {type: Date, default: null},  //null if rolling
-            startDate: '', //null if start asap, string b/c it will prob be something like Fall 2018
-            minSemesters: 0,   //can be null, indicating no min. minimum number of semesters they're expected to work in the lab
-            minHours: 0, //can be null, indicating no minimum
-            maxHours: 0, //can be null, indicating no max
-            qualifications: '', //can be null/empty
-            minGPA: 0, //0 if no minimum gpa required
-            requiredClasses: [], //can be empty
-            questions: [],    //can be empty
-            yearsAllowed: [],  //required, do they accept freshman, sophomores, juniors, and/or seniors
-            //applications: 0,   //number of people who've submitted, default 0, they don't submit this
-            spots: 0
-        };
+        // this.state = {
+        //     title: '',
+        //     area: [], //required, area(s) of research (molecular bio, bioengineering, electrical engineering, computer science, etc.)
+        //     labName: '',    //required
+        //     labId: '',  //required
+        //     pi: '', //required
+        //     supervisor: '', //can be null
+        //     projectDescription: '', //required, add min length that you see fit
+        //     undergradTasks: '',  //what the undergrad would be doing, can be null
+        //     opens: {type: Date, default: new Date()},   //if no date is sent use new Date()
+        //     closes: {type: Date, default: null},  //null if rolling
+        //     startDate: '', //null if start asap, string b/c it will prob be something like Fall 2018
+        //     minSemesters: 0,   //can be null, indicating no min. minimum number of semesters they're expected to work in the lab
+        //     minHours: 0, //can be null, indicating no minimum
+        //     maxHours: 0, //can be null, indicating no max
+        //     qualifications: '', //can be null/empty
+        //     minGPA: 0, //0 if no minimum gpa required
+        //     requiredClasses: [], //can be empty
+        //     questions: [],    //can be empty
+        //     yearsAllowed: [],  //required, do they accept freshman, sophomores, juniors, and/or seniors
+        //     //applications: 0,   //number of people who've submitted, default 0, they don't submit this
+        //     spots: 0
+        // };
+        this.state = OpportunityJSON;
     }
 
     contains(needle) {
