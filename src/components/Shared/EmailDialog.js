984b52b7dbcd4c2f16df6c95c1db184361112208
Phillip Kheyfets <phillipkheyfets@gmail.com>
2018-03-15 20:30:55 -0400

    sending feature implemented, there is a bug. see modal text after send

--- src/components/Shared/EmailDialog.js
+++ src/components/Shared/EmailDialog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
+import axios from 'axios';
 
 const customStyles = {
 	content : {
@@ -9,7 +10,8 @@ const customStyles = {
 		right                 : 'auto',
 		bottom                : 'auto',
 		marginRight           : '-50%',
-		transform             : 'translate(-50%, -50%)'
+		transform             : 'translate(-50%, -50%)',
+		width									: '50%'
 	}
 };
 
@@ -18,7 +20,8 @@ class EmailDialog extends React.Component {
 		super();
 
 		this.state = {
-			modalIsOpen: false
+			modalIsOpen: false,
+			emailContent: ''
 		};
 
 		this.openModal = this.openModal.bind(this);
@@ -28,6 +31,24 @@ class EmailDialog extends React.Component {
 
 	openModal() {
 		this.setState({modalIsOpen: true});
+		axios.get('/messages/' + this.props.opp._id, {
+			'studentFirstName': this.props.app.firstName,
+			'studentLastName': this.props.app.lastName,
+			'opportunityTitle': this.props.opp.title,
+			'yourFirstName': this.props.opp.supervisor,
+			'yourLastName': this.props.opp.supervisor,
+			'yourEmail': this.props.opp.creatorNetId
+		})
+		.then((response) => {
+			console.log(response);
+			this.setState({ 
+				emailContent: response.data[this.props.buttonText.toLowerCase()],
+			});
+			console.log(this.state.emailContent);
+		})
+		.catch(function (error) {
+			console.log(error);
+		})
 	}
 
 	afterOpenModal() {
@@ -39,24 +60,61 @@ class EmailDialog extends React.Component {
 		this.setState({modalIsOpen: false});
 	}
 
+	sendEmail() {
+		axios.post('/messages/send', {
+			'opportunityId': this.props.opp._id,
+			'labAdminNetId': this.props.opp.creatorNetId,
+			'undergradNetId': this.props.app.undergradNetId,
+			'message': this.state.emailContent,
+			'status': this.props.buttonText.toLowerCase()
+		}).then(function (response) {
+        //handle success
+        console.log(response);
+    })
+		.catch(function (error) {
+			console.log(error);
+		})
+	}
+
+	getDisabled() {
+		let action = this.props.buttonText.toLowerCase();
+		let status = this.props.app.status;
+		if (status === 'accepted' || status === 'rejected') {
+			return true;
+		}
+		if (status === 'interviewing') {
+			if (action === 'interview') {
+				return true;
+			} else {
+				return false;
+			}
+		}
+	}
+
 	render() {
 		return (
 			<div>
-				<a className="button" onClick={this.openModal}>{ this.props.buttonText }</a>
+				<button className="button"
+				   disabled={ this.getDisabled() } 
+				   onClick={ this.openModal }
+				>
+				   { this.props.buttonText }
+				</button>
 				<Modal
+					ariaHideApp={false}
 					isOpen={this.state.modalIsOpen}
 					onAfterOpen={this.afterOpenModal}
 					onRequestClose={this.closeModal}
 					style={customStyles}
-					contentLabel="Example Modal"
+					contentLabel="Modal Content"
 				>
-
-					<h2 ref={subtitle => this.subtitle = subtitle}>Hello</h2>
-					<button onClick={this.closeModal}>close</button>
-					<div>I am a modal</div>
-					<form>
-						<input />
-					</form>
+					<div className="modal">
+						<h2 ref={subtitle => this.subtitle = subtitle}>Email Student</h2>
+						<label>Message</label>
+						<div>{ this.state.emailContent }</div>
+						<button className="submit-button" onClick={this.sendEmail.bind(this)}>Submit</button>
+						<button className="close-button" onClick={this.closeModal}>Close</button>
+					</div>
 				</Modal>
 			</div>
 		);
