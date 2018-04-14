e53df33102f0edd27b8c1f8dea95c54ba9c44967
Aram Baghdassarian <abagh0703@gmail.com>
2018-04-14 15:57:29 -0400

    mongoose is now defined

--- api/applications.js
+++ api/applications.js
@@ -1,6 +1,6 @@
 let express = require('express');
 let app = express.Router();
-let {undergradModel, labAdministratorModel, opportunityModel, labModel, debug, replaceAll, sgMail, decryptGoogleToken} = require('../common.js');
+let {undergradModel, labAdministratorModel, opportunityModel, labModel, debug, replaceAll, sgMail, decryptGoogleToken, mongoose} = require('../common.js');
 
 /**
  * Send a request to /applications/:id, where "id" is the id of the application
