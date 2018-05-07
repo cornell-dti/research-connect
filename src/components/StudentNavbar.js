82106a39ac76c153c030d98da193a9cde521b39d
Rachel Susanne Nash <rsn55@cornell.edu>
2018-05-06 20:30:43 -0400

    navbar logo

--- src/components/StudentNavbar.js
+++ src/components/StudentNavbar.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../Navbar.css';
 import logo from '../images/wordlogo.png';
+import curblogo from '../images/CURB.png';
 import { Link } from 'react-router-dom';
 
 class StudentNavbar extends Component {
@@ -22,7 +23,11 @@ class StudentNavbar extends Component {
   render() {
 		return (
       <div className="header-all">
+				<div className="logo-div">
         <Link to="opportunities"><img className="logo" src={logo}/></Link>
+				<p className="partnership">in partnership with</p>
+				<a href="http://curb.cornell.edu/"><img className="logo" src={curblogo}/></a>
+				</div>
         <nav>
           <li><a href={window.location.href.toString().slice(-1) === "/" ? window.location.href.toString().substring(0, window.location.href.toString().length).replace(/\/[^\/]*$/, '/opportunities') : ""}>Opportunities</a></li>
           <li><a className="sign-out" onClick={this.logout.bind(this)}>Sign Out</a></li>
