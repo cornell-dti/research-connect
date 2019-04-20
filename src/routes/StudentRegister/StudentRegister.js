cc6cce6346f7e5a6d12af5b3544df9e4bd782d46
connielei <callmecon328@gmail.com>
2019-04-20 12:36:08 -0400

    cleanup

--- src/routes/StudentRegister/StudentRegister.js
+++ src/routes/StudentRegister/StudentRegister.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import '../App/App.scss';
 import Dropzone from 'react-dropzone';
-import Navbar from '../../components/Navbars/StudentNavbar/StudentNavbar.js';
 import Footer from '../../components/Footer/Footer';
 import CourseSelect from '../../components/CourseSelect/CourseSelect';
 import './StudentRegister.scss';
@@ -14,7 +13,7 @@ ReactGA.pageview(window.location.pathname + window.location.search);
 const majorList = Utils.getMajorList();
 const gradYears = [new Date().getFullYear(), new Date().getFullYear() + 1, new Date().getFullYear() + 2, new Date().getFullYear() + 3, new Date().getFullYear() + 4];
 
-class StudentRegister extends React.Component {
+class StudentRegister extends Component {
   constructor(props) {
     super(props);
     this.state = {
